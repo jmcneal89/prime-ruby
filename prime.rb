8a707281ebb412d26707d2a55d1ba11c49e1a556
jmcneal89 <jmcneal.writer@gmail.com>
2021-01-24 16:27:53 -0600

    commit tree 2a01547839321c5602bcf08e45afdb77b874583b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(number)
+    if number < 2 || number % 2 == 0 && number != 2
+        return false
+    else
+        (2..number-1).map.all? { |num| number % num != 0 }
+    end     
+end
